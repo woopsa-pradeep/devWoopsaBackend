8be4fa9f8fb96870d8cbebf2a23f78458143a1bb
pradeep <negipradeep896@gmail.com>
2025-11-11 22:14:30 +0000

    add

--- src/businesslogic/sales.service.ts
+++ src/businesslogic/sales.service.ts
@@ -222,6 +222,8 @@ export class SalesService {
       console.log(item.Price, 'item.Price-->')
 
       console.log(hasDiscount == true,'hasDiscount == true')
+
+      let adjprice = hasDiscount == true? Number(item.discountPrice || 0): Number(item.Price);
       const orderDetail = {
         Order_Number: orderHeaderCreated.Order_Number,
         Item_Number: item.Item_Number,
@@ -231,8 +233,8 @@ export class SalesService {
         Quantity_Ordered: Number(item.Qty),
         Quantity_Shipped: item.Qty,
         Pack: product.Pack,
-        Price: hasDiscount == true? Number(item.discountPrice || 0): Number(item.Price),
-        Price_Reference:hasDiscount == true? Number(item.discountPrice || 0): Number(item.Price),
+        Price: Number(adjprice),
+        Price_Reference:Number(adjprice),
         Retail: product.Retail1,
         NetCost: product.NetCost,
         BaseCost: product.BaseCost,
