bb44ec0d7a457eee9c2923e77f4ebc597fbf6d09
pradeep <negipradeep896@gmail.com>
2025-11-23 23:15:39 +0000

    add

--- src/utils/order.ts
+++ src/utils/order.ts
@@ -73,7 +73,7 @@ export const getDefaultOrderValues = (): OrderDefaultValues => {
     POS_CardFee: 0,
     POS_PaidOnAccount: 0,
     AssignedToEPickUser: null,
-    AssignedOnforEPick:dateTimeObject,
+    // AssignedOnforEPick:dateTimeObject,
     EpickStatusFromPicker: null,
     EpickCompletedOn: null,
     OrderedOn: null,
