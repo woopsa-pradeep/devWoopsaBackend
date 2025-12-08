d83318d2762a290dd7e9e41e385a14c3649ea58e
pradeep <negipradeep896@gmail.com>
2025-12-08 20:11:31 +0000

    add

--- src/models/mmsql/inventory.model.ts
+++ src/models/mmsql/inventory.model.ts
@@ -143,8 +143,8 @@ export class Inventory extends Model {
   public AddOnDeposit_Item_Number!: number;
   public IsIncludeDeposit_QB!: boolean;
   public MinimumStockAvailability!: number;
-  // public ItemExpiryDate?: string;
-  public Item_ExpiryDate?: string;
+  public ItemExpiryDate?: string;
+  // public Item_ExpiryDate?: string;
   public UPCList?: InventoryUPC[];
   public SalesCategory?: SalesCategory;
   public PriceClass?: PriceClass;
@@ -300,7 +300,7 @@ Description: {
     AddOnDeposit_Item_Number: DataTypes.INTEGER,
     IsIncludeDeposit_QB: DataTypes.BOOLEAN,
     MinimumStockAvailability: DataTypes.DECIMAL,
-    Item_ExpiryDate: DataTypes.STRING,
+    ItemExpiryDate: DataTypes.STRING,
   },
   {
     sequelize,
