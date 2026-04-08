6880c8e1260a26809ee2ccc8aed40cdd5eea3280
pradeep <negipradeep896@gmail.com>
2026-04-08 17:47:50 +0000

    add
    add

--- src/businesslogic/driver.service.ts
+++ src/businesslogic/driver.service.ts
@@ -270,7 +270,7 @@ export class DriverService {
           );
 
       const isCompleteRoute = !stops.some(
-        (s) => s.status === DeliveryStopStatus.IN_PROGRESS || s.status === DeliveryStopStatus.NOT_DELIVERED || s.status === DeliveryStopStatus.FAILED || s.status === DeliveryStopStatus.RETURNED || s.status === DeliveryStopStatus.CANCELLED
+        (s) => s.status === DeliveryStopStatus.IN_PROGRESS || s.status === DeliveryStopStatus.NOT_DELIVERED
       );
 
       return {
@@ -1725,13 +1725,6 @@ export class DriverService {
       cancelledAt: new Date(),
     });
 
-    if (stop.isLastStop) {
-      await DeliveryRoute.update({
-        routeStatus: RouteStatus.COMPLETED,
-      }, { where: { id: stop.routeId } });
-
-
-    }
 
     const updateNextStop = await DeliveryRouteStop.findOne({
       where: { routeId: stop.routeId, isActive: true, stopSequence: stop.stopSequence + 1 },
--- src/models/postgres/index.ts
+++ src/models/postgres/index.ts
@@ -9,6 +9,7 @@ export const syncPostgresModels = async () => {
   try {
 
      await postgresSequelize.sync({ alter: true });
+
     console.log('PostgreSQL models synchronized successfully');
   } catch (error) {
     console.error('Error synchronizing PostgreSQL models:', error);
