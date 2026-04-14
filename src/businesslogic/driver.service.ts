6b609f99508ebd00f969cbfe07ea952c96e61fa1
pradeep <negipradeep896@gmail.com>
2026-04-14 13:36:15 +0000

    add
    add

--- src/businesslogic/driver.service.ts
+++ src/businesslogic/driver.service.ts
@@ -1797,13 +1797,13 @@ export class DriverService {
     );
 
     const ALLOWED_RADIUS_METERS = 100;
-    if (distanceInMeters > ALLOWED_RADIUS_METERS) {
-      throw new AppError(
-        `You are ${Math.round(distanceInMeters)}m away from the stop. Must be within ${ALLOWED_RADIUS_METERS}m to complete delivery.`,
-        400
-      );
+    // if (distanceInMeters > ALLOWED_RADIUS_METERS) {
+    //   throw new AppError(
+    //     `You are ${Math.round(distanceInMeters)}m away from the stop. Must be within ${ALLOWED_RADIUS_METERS}m to complete delivery.`,
+    //     400
+    //   );
 
-    }
+    // }
     return true;
 
 
